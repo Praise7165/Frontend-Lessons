9ed909c086d00b082c7dd0d1e71ef6d6f26bfc96
Omolade Lekan <omoladelekan99@gmail.com>
2026-03-05 04:48:12 +0100

    started learning typescript

---
+++ Learn typescript/script.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const obj = { width: 10, height: 15 };
+const area = obj.width * obj.height;
+console.log(area);
+//# sourceMappingURL=script.js.map
