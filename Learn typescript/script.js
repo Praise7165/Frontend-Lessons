b86bfdf60673de8daafeeda5164f709bbae883b3
Omolade Lekan <omoladelekan99@gmail.com>
2026-03-09 19:01:23 +0100

    learning ts for today

--- Learn typescript/script.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const x = 23;
-console.log(x);
-//# sourceMappingURL=script.js.map
--- Learn typescript/script.ts
+++
@@ -1,3 +0,0 @@
-const x: number = 23;
-
-console.log(x);
